87a438a4cba63a4ec91a9fb0de7007dec24544f7
sreebhavana475-coder <sreebhavana475@gmail.com>
2026-02-04 18:36:26 +0530

    Implement service worker registration
    Added service worker registration on page load.

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 window.onload = function () {
     console.log("Portfolio loaded successfully");
 };
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('service-worker.js')
+      .then(() => console.log('Service Worker registered'))
+      .catch(err => console.log('SW failed', err));
+  });
+}
