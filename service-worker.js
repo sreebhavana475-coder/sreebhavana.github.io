96d96a943fa61937650d1fa4efb8cccad43ec299
sreebhavana475-coder <sreebhavana475@gmail.com>
2026-02-04 18:39:47 +0530

    Implement service worker for caching assets

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "sree-portfolio-cache-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./profile.jpg"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
